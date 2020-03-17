af070e3becc7e2523f2ee11677c19f64d8f9afb1
raul-avila-altran <62135761+raul-avila-altran@users.noreply.github.com>
2020-03-17 20:50:29 +0100

    End of day
    Added some features

--- src/InitializeFromStateForm.js
+++ src/InitializeFromStateForm.js
@@ -1,101 +1,241 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
-import { load as loadAccount } from './account';
+import React from "react";
+import { connect } from "react-redux";
+import { Field, FieldArray, reduxForm } from "redux-form";
+import { load as loadAccount } from "./account";
 const data = {
-  // used to populate "account" reducer when "Load" is clicked
-  firstName: 'Jane',
-  lastName: 'Doe',
-  age: '42',
-  sex: 'female',
-  employed: true,
-  favoriteColor: 'Blue',
-  bio: 'Born to write amazing Redux code.',
+  // used to populate "workflow" reducer when "Load" is clicked
+  name: "transaction_created_or_modified",
+  type: "subscription",
+  removeAtEnd: "true",
+  tasks: [{
+    name: "retrieve_policy",
+    type: "retrieve_policy",
+    waitForReturn: "false",
+    queue: "salesforce",
+    options: {
+      check_policy_status_not: "DRAFT"
+    },
+    dependsOn:[]
+  },{
+    name: "update_policy_balance",
+    type: "update_policy_balance",
+    waitForReturn: "false",
+    queue: "salesforce",
+    options: {
+      singleParameter: "FALSE"
+    },
+    dependsOn:[
+      "retrieve_policy"
+    ]
+  }
+]
 };
-const colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+const options = ["check_policy_status_not", "singleParameter", "withClaims", "type", "check_policy_status", "DRAFT_CERTIFICATE", "PAYMENT_MANAGED_BY_AXA", "IDD_COMPLIANT"];
 
+const renderTask = ({ fields, meta: { touched, error, submitFailed } }) => (
+  <ul>
+    <li>
+      <button type="button" onClick={() => fields.push({})}>
+        Add Task
+      </button>
+      {(touched || submitFailed) && error && <span>{error}</span>}
+    </li>
+    {fields.map((task, index) => (
+      <li key={index}>
+        <div>
+        <h4>Task #{index + 1}</h4>
+        <button
+          type="button"
+          title="Remove Task"
+          onClick={() => fields.remove(index)}
+        />
+        </div>
+
+        <div>
+        <label>Name</label>
+        <div>
+          <Field
+            name={`${task}.name`}
+            component="input"
+            type="text"
+            placeholder="Task Name"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Type</label>
+        <div>
+          <Field
+            name={`${task}.type`}
+            component="input"
+            type="text"
+            placeholder="Type Name"
+          />
+        </div>
+      </div>   
+         <div>
+          <label>Wait For Return</label>
+          <div>
+          <label>
+            <Field
+              name={`${task}.waitForReturn`}
+              component="input"
+              type="radio"
+              value="true"
+            />{" "}
+            True
+          </label>
+          <label>
+            <Field
+              name={`${task}.waitForReturn`}
+              component="input"
+              type="radio"
+              value="false"
+            />{" "}
+            False
+           </label>
+          </div>
+        </div>
+        <div>
+        <label>Queue</label>
+        <div>
+          <Field
+            name={`${task}.queue`}
+            component="input"
+            type="text"
+            placeholder="Queue"
+          />
+        </div>
+      </div>  
+      <div>
+      <FieldArray name={`${task}.options`} component={renderOptions} />
+
+      </div>
+      <FieldArray name={`${task}.dependsOn`} component={renderDependancy} />
+
+        {/* <FieldArray name={`${task}.dependsOn`} component={renderDependancy} /> */}
+      </li>
+    ))}
+  </ul>
+);
+
+const renderDependancy = ({ fields, meta: { error } }) => (
+  <ul>
+    <li>
+      <button type="button" onClick={() => fields.push()}>
+        Add Dependancy
+      </button>
+    </li>
+    {fields.map((dependancy, index) => (
+      <li key={index}>
+        <button
+          type="button"
+          title="Remove Dependancy"
+          onClick={() => fields.remove(index)}
+        />
+        <div>
+        <label>{`Dependancy #${index + 1}`}</label>
+        <div>
+          <Field
+            name={dependancy}
+            component="input"
+            type="text"
+            placeholder="Depends On"
+          />
+        </div>
+      </div>  
+      </li>
+    ))}
+    {error && <li className="error">{error}</li>}
+  </ul>
+);
+
+const renderOptions = ({ fields, meta: { error } }) => (
+  <ul>
+    <li>
+     <button type="button" onClick={() => fields.push({})}>
+        Add Task
+      </button>
+    </li>
+    {options.map((option, index) => (
+      <li key={index}>
+        <div>
+        <label>{`${fields.name}`}.{option}</label>
+        <div>
+          <Field
+            name={option}
+            component="input"
+            type="text"
+            placeholder={option}
+          />
+        </div>
+      </div>  
+      </li>
+    ))}
+    {error && <li className="error">{error}</li>}
+  </ul>
+);
 let InitializeFromStateForm = props => {
   const { handleSubmit, load, pristine, reset, submitting } = props;
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <button type="button" onClick={() => load(data)}>Load Account</button>
+        <button type="button" onClick={() => load(data)}>
+          Load Account
+        </button>
       </div>
       <div>
-        <label>First Name</label>
+        <label>Name</label>
         <div>
           <Field
-            name="firstName"
+            name="name"
             component="input"
             type="text"
-            placeholder="First Name"
+            placeholder="Workflow Name"
           />
         </div>
       </div>
       <div>
-        <label>Last Name</label>
+        <label>Type</label>
         <div>
           <Field
-            name="lastName"
+            name="type"
             component="input"
             type="text"
-            placeholder="Last Name"
+            placeholder="Workflow Type"
           />
         </div>
       </div>
       <div>
-        <label>Age</label>
-        <div>
-          <Field name="age" component="input" type="number" placeholder="Age" />
-        </div>
-      </div>
-      <div>
-        <label>Sex</label>
+        <label>Remove At End</label>
         <div>
           <label>
-            <Field name="sex" component="input" type="radio" value="male" />
-            {' '}
-            Male
+            <Field
+              name="removeAtEnd"
+              component="input"
+              type="radio"
+              value="true"
+            />{" "}
+            True
           </label>
           <label>
-            <Field name="sex" component="input" type="radio" value="female" />
-            {' '}
-            Female
+            <Field
+              name="removeAtEnd"
+              component="input"
+              type="radio"
+              value="false"
+            />{" "}
+            False
           </label>
         </div>
       </div>
       <div>
-        <label>Favorite Color</label>
-        <div>
-          <Field name="favoriteColor" component="select">
-            <option value="">Select a color...</option>
-            {colors.map(colorOption => (
-              <option value={colorOption} key={colorOption}>
-                {colorOption}
-              </option>
-            ))}
-          </Field>
-        </div>
+        <FieldArray name="tasks" component={renderTask} />
       </div>
       <div>
-        <label htmlFor="employed">Employed</label>
-        <div>
-          <Field
-            name="employed"
-            id="employed"
-            component="input"
-            type="checkbox"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Bio</label>
-        <div>
-          <Field name="bio" component="textarea" />
-        </div>
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>Submit</button>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
         <button type="button" disabled={pristine || submitting} onClick={reset}>
           Undo Changes
         </button>
@@ -106,15 +246,15 @@ let InitializeFromStateForm = props => {
 
 // Decorate with reduxForm(). It will read the initialValues prop provided by connect()
 InitializeFromStateForm = reduxForm({
-  form: 'initializeFromState', // a unique identifier for this form
+  form: "initializeFromState" // a unique identifier for this form
 })(InitializeFromStateForm);
 
 // You have to connect() to any reducers that you wish to connect to yourself
 InitializeFromStateForm = connect(
   state => ({
-    initialValues: state.account.data, // pull initial values from account reducer
+    initialValues: state.account.data // pull initial values from account reducer
   }),
-  { load: loadAccount }, // bind account loading action creator
+  { load: loadAccount } // bind account loading action creator
 )(InitializeFromStateForm);
 
 export default InitializeFromStateForm;
