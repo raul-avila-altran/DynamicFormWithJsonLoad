072664c9f0a9d2dd304c3c4fd5ed5872325ec1b8
raul-avila-altran <62135761+raul-avila-altran@users.noreply.github.com>
2020-03-31 00:13:02 +0200

    upload feature added

--- src/InitializeFromStateForm.js
+++ src/InitializeFromStateForm.js
@@ -1,47 +1,24 @@
-import React, {useCallback} from 'react'
-import { connect } from "react-redux";	
-import { FieldArray, reduxForm } from "redux-form";	
-import { load as loadAccount } from "./account";	
-import {updateTask } from "./account";	
+import React, { useCallback } from "react";
+import { connect } from "react-redux";
+import { FieldArray, reduxForm } from "redux-form";
+import { load as loadAccount } from "./account";
+import { updateTask } from "./account";
 import renderWorkflow from "./components/workflow";
 import workflowFile from "./workflow";
-import {useDropzone} from 'react-dropzone'
-function MyDropzone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log("acceptedFilres");
-    console.log(acceptedFiles)
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
-      console.log("file")
-      console.log(file)
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onloadens = () => {
-      // Do whatever you want with the file contents
-        console.log(reader.result)
-        const binaryStr = JSON.parse(reader.result)
-        console.log("Test2")
-        console.log(binaryStr)
-      }     
-       console.log(reader.readAsDataURL(file));
-
-    })
-    
-  }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
-
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
-    </div>
-  )
-}
-
-const options = ["check_policy_status_not", "singleParameter", "withClaims", "type", "check_policy_status", "DRAFT_CERTIFICATE", "PAYMENT_MANAGED_BY_AXA", "IDD_COMPLIANT"];	
+import MyDropzone from "./components/dropzone";
+const options = [
+  "check_policy_status_not",
+  "singleParameter",
+  "withClaims",
+  "type",
+  "check_policy_status",
+  "DRAFT_CERTIFICATE",
+  "PAYMENT_MANAGED_BY_AXA",
+  "IDD_COMPLIANT"
+];
 
 let InitializeFromStateForm = props => {
-  const { handleSubmit, load, pristine, reset, submitting} = props;
+  const { handleSubmit, load, pristine, reset, submitting } = props;
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -49,6 +26,7 @@ let InitializeFromStateForm = props => {
           Load Workflow
         </button>
 
+        <MyDropzone />
       </div>
       <div>
         <FieldArray name="workflows" component={renderWorkflow} />
@@ -65,20 +43,17 @@ let InitializeFromStateForm = props => {
   );
 };
 
+// Decorate with reduxForm(). It will read the initialValues prop provided by connect()
+InitializeFromStateForm = reduxForm({
+  form: "initializeFromState" // a unique identifier for this form
+})(InitializeFromStateForm);
 
-// Decorate with reduxForm(). It will read the initialValues prop provided by connect()	
-InitializeFromStateForm = reduxForm({	
-  form: "initializeFromState" // a unique identifier for this form	
-})(InitializeFromStateForm);	
-
-// You have to connect() to any reducers that you wish to connect to yourself	
-InitializeFromStateForm = connect(	
-  state => ({	
-    initialValues: state.account.data // pull initial values from account reducer	
-  }),	
-  { load: loadAccount,
-    update: updateTask } // bind account loading action creator	
- 
-)(InitializeFromStateForm);	
+// You have to connect() to any reducers that you wish to connect to yourself
+InitializeFromStateForm = connect(
+  state => ({
+    initialValues: state.account.data // pull initial values from account reducer
+  }),
+  { load: loadAccount, update: updateTask } // bind account loading action creator
+)(InitializeFromStateForm);
 
 export default InitializeFromStateForm;
--- src/components/dropzone.js
+++ src/components/dropzone.js
@@ -1,28 +1,37 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { connect } from "react-redux";
+import { load as loadAccount } from "../account";
 
-function MyDropzone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
-
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
+let MyDropzone = props => {
+  const { load } = props;
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.forEach(file => {
+      const reader = new FileReader();
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-      // Do whatever you want with the file contents
-        const binaryStr = reader.result
-        console.log(binaryStr)
-      }
-      reader.readAsArrayBuffer(file)
-    })
-    
-  }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
+        const binaryStr = JSON.parse(reader.result);
+        load(binaryStr);
+      };
+      console.log(reader.readAsText(file));
+    });
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
+      <p>Drag 'n' drop some workflow files here, or click to select them</p>
     </div>
-  )
-}
+  );
+};
+// You have to connect() to any reducers that you wish to connect to yourself
+MyDropzone = connect(
+  state => ({
+    initialValues: state.account.data // pull initial values from account reducer
+  }),
+  { load: loadAccount } // bind account loading action creator
+)(MyDropzone);
+
+export default MyDropzone;
